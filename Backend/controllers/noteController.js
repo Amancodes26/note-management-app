9029ae7ad33af07786e084955e0e520a553ac64a
DevanshChhabra <devanshchhabra2020@gmail.com>
2024-08-07 12:01:57 +0530

    Update noteController.js

--- Backend/controllers/noteController.js
+++ Backend/controllers/noteController.js
@@ -51,17 +51,23 @@ exports.updateNote = async (req, res) => {
 
 // Delete a note
 exports.deleteNote = async (req, res) => {
+
     try {
+        console.log('Deleting note with ID:', req.params.id);
         const note = await Note.findById(req.params.id);
         if (!note) {
+            console.log('Note not found');
             return res.status(404).json({ msg: 'Note not found' });
         }
         if (note.user.toString() !== req.user.id) {
+            console.log('User not authorized');
             return res.status(401).json({ msg: 'User not authorized' });
         }
-        await note.remove();
+        await Note.findByIdAndDelete(req.params.id);
+        console.log('Note removed');
         res.json({ msg: 'Note removed' });
     } catch (error) {
+        console.error('Server error:', error.message);
         res.status(500).json({ message: 'Server Error' });
     }
 };
